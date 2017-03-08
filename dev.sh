336feb442c61cec7b07944d19e5ff80eae1591a3
YLWS-4617 <wmy461737664@qq.com>
2017-03-08 15:15:57 +0800

    增加一键封禁BT，垃圾邮件

--- dev.sh
+++ dev.sh
@@ -6,10 +6,10 @@ export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 echo "测试区域，请勿随意使用"
 echo "1.更新SSR-Bsah"
-
+echo "2.一键封禁BT下载，SPAM邮件流量（无法撤销）"
 while :; do echo
 read -p "请选择： " devc
-if [[ ! $devc =~ ^[1-1]$ ]]; then
+if [[ ! $devc =~ ^[1-2]$ ]]; then
 	echo "${CWARNING}输入错误! 请输入正确的数字!${CEND}"
 else
 	break	
@@ -24,4 +24,8 @@ if [[ $devc == 1 ]];then
 	mv /usr/local/SSR-Bash-Python/ssr /usr/local/bin/
 	chmod +x /usr/local/bin/ssr
 	echo 'SSR-Bash升级成功！'
+fi
+
+if [[ $devc == 2 ]];then
+	wget -4qO- softs.pw/Bash/Get_Out_Spam.sh|bash
 fi
