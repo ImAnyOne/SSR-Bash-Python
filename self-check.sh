7dcbe879ea61373f325dbc975af2e0fdd038332b
KiritoMiao <i@ixh.me>
2017-04-13 23:30:33 +0800

    增加自检功能

---
+++ self-check.sh
@@ -0,0 +1,56 @@
+#/bin/sh
+#Check Root
+[ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
+echo "##################################
+      SSR-Bash-Python 自检系统
+             V1.0 Alpha
+           Author:Kirito
+##################################"
+#List /usr/local
+echo "############Filelist of /usr/local" >> /root/report.json
+cd /usr/local
+ls >> /root/report.json
+#List /usr/local/ssr-bash-python
+echo "############Filelist of /usr/local/SSR-Bash-Python" >> /root/report.json
+cd /usr/local/SSR-Bash-Python
+ls >> /root/report.json
+#List /usr/local/shadowsockr
+echo "############Filelist of /usr/local/shadowsockr" >> /root/report.json
+cd /usr/local/shadowsocksr
+ls >> /root/report.json
+echo "############File test">>/root/report.json
+#Check File Exist
+if [ ! -f "/usr/local/bin/ssr" ]; then
+  echo "SSR-Bash-Python主文件缺失，请确认服务器是否成功连接至Github"
+  echo "SSR Miss" >> /root/report.json
+  exit
+fi
+if [ ! -f "/usr/local/SSR-Bash-Python/server.sh" ]; then
+  echo "SSR-Bash-Python主文件缺失，请确认服务器是否成功连接至Github"
+  echo "SSR Miss" >> /root/report.json
+  exit
+fi
+if [ ! -f "/usr/local/shadowsocksr/stop.sh" ]; then
+  echo "SSR主文件缺失，请确认服务器是否成功连接至Github"
+  echo "SSR Miss" >> /root/report.json
+  exit
+fi
+#Network Test
+echo "############Intelnet Test" >> /root/report.json
+cd /root
+curl http://myip.ipip.net
+curl http://myip.ipip.net >> /root/report.json
+curl -o test.net https://www.google.com
+if [ ! -f "/root/test.net" ]; then
+  echo "Intelnet Error，请确认服务器是否联网"
+  echo "Intelnet Miss" >> /root/report.json
+  exit
+fi
+ping www.google.com -c 5 >> /root/report.json
+ping www.facebook.com -c 5 >> /root/report.json
+
+#Firewall
+echo "############Firewall test" >> report.json
+iptables -L >> /root/report.json
+
+echo "检测完成，未发现严重问题，如仍有任何问题请提交report.json"
